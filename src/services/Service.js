d5057e464e84f1d0dd66fe2e293c7333fd6981fe
子清 <120438383+ziqing888@users.noreply.github.com>
2025-02-28 10:06:08 +0800

    Create Service.js

---
+++ src/services/Service.js
@@ -0,0 +1,13 @@
+const BaseService = require("./BaseService");
+const StakingService = require("./StakingService");
+const SwapService = require("./SwapService");
+const TokenService = require("./TokenService");
+const BeanswapService = require("./BeanswapService");
+
+module.exports = {
+  BaseService, 
+  StakingService, 
+  SwapService, 
+  TokenService,
+  BeanswapService, 
+};
