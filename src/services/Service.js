8221712e1a14f71bf9b35cb9a0cf6530914bce2e
子清 <120438383+ziqing888@users.noreply.github.com>
2025-03-02 00:08:32 +0800

    Update Service.js

--- src/services/Service.js
+++ src/services/Service.js
@@ -3,11 +3,13 @@ const StakingService = require("./StakingService");
 const SwapService = require("./SwapService");
 const TokenService = require("./TokenService");
 const BeanswapService = require("./BeanswapService");
+const KuruSwapService = require("./KuruSwapService");
 
 module.exports = {
-  BaseService, 
-  StakingService, 
-  SwapService, 
+  BaseService,
+  StakingService,
+  SwapService,
   TokenService,
-  BeanswapService, 
+  BeanswapService,
+  KuruSwapService,
 };
